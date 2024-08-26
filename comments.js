afb84271215d0b0d674cfd0d62590cc545540be7
sknull-dej <stephan.knull@dej-technology.de>
2024-08-26 06:33:24 +0000

    Copilot third commit,

---
+++ comments.js
@@ -0,0 +1,21 @@
+// create webserver
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// create server
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+
+console.log('Server running at http://0.0.0.0:8080/');
